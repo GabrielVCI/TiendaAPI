c2a62a196e19be748c23d00e7a6e5b254af38170
Gabriel Valdez <GabrielVCI>
2024-11-07 14:54:03 -0400

    FT-04-CreacionDeMetodos
    He creado todos los métodos HTTP de la API, así mismo, creé cada uno de los métodos del context para hacer las peticiones a la base de datos, configuré el MappingProfile para usar un DTO en vez del modelo por motivos de seguridad, y terminé de configurar la clase program.

--- Interfaces/IProductosRepositoio.cs
+++ Interfaces/IProductosRepositoio.cs
@@ -1,6 +1,15 @@
-﻿namespace TiendaPruebaAPI.Interfaces
+﻿using TiendaPruebaAPI.Models;
+
+namespace TiendaPruebaAPI.Interfaces
 {
-    public class IProductosRepositoio
+    public interface IProductosRepositoio
     {
+        Task<List<Productos>> ListadoProductos();
+        Task<Productos> GetProducto(int productoId);
+        Task<bool> EditarProducto(Productos producto);
+        Task<bool> EliminarProducto(int productoId);
+        Task<bool> Save();
+        Task<bool> ProductoExiste(int productoId);
+        Task<bool> GuardarProducto(Productos producto); 
     }
 }
